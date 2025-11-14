b60659512788e2faaf7517a5cffb343105b5292f
= <=>
2025-11-14 10:50:08 +0100

    Empezamos con POO en JS 14/11/2025

---
+++ Tarea 7/ej1.js
@@ -0,0 +1,8 @@
+//Crear un objeto person con nombre, edad y ciudad. Acceder  asus porpiedades con . y con []. Cambiar el valor 
+//de edad y agregar un nueva propiedad profesión
+
+// class Persona {
+//     // constructor
+// }
+
+//Falta hacer 
