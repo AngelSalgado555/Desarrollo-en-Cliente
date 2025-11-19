f57b8614b9aa37abc766843abf7d01401fa2750a
= <=>
2025-11-19 14:20:03 +0100

    Casi esta 19/11/2025

--- Tarea 7/ej4.js
+++ Tarea 7/ej4.js
@@ -5,13 +5,26 @@ students.set("Josue", 9);
 
 function aprobados(students){
     let clavesAprobadas = new Array();
-    const claves = Array.from(students.keys());
-    for (let i = 0; i < claves.length; i++) {
-       if (claves[i] > 5) {
-            clavesAprobadas.push(claves[i]);
-       }
+    for (let [clave, valor] of students){
+        if (valor > 5){
+            clavesAprobadas.push(clave + " ha aprobado con: " + valor);
+        }
     }
     return clavesAprobadas;
 }
 
 console.log(aprobados(students));
+
+function searchByName(name){
+    let nameIgual = "";
+    const claves = Array.from(students.keys());
+    for (let i = 0; i < claves.length; i++) {
+        if (claves[i] == name){
+            nameIgual = name;
+        } 
+    }
+    return nameIgual;
+}
+
+let nombre = "Pepe";
+console.log("El nombre es:" + searchByName(nombre));
