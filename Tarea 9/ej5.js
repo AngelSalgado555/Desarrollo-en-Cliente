2e3322485b321590e275f3bd007a576718311869
= <=>
2025-12-17 14:27:18 +0100

    Falta ver el ultimo, aun no esta bien 17/12/2025

--- Tarea 9/ej5.js
+++ Tarea 9/ej5.js
@@ -1,1 +1,10 @@
-let medio = document.getElementById("medio");
+medio = document.getElementById("medio");
+let antes = medio.previousElementSibling;
+
+let despues = medio.nextElementSibling;
+if (medio == "Pablo"){
+    console.log("Antes: " + antes);
+    console.log("Después: " + despues);
+}
+
+
